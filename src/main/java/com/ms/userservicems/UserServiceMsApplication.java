34e39be6f5db1f3c64e181918af00d0e80da6bdc
vipul76 <vipulnilesh76@gmail.com>
2024-02-17 22:04:30 +0530

    initial commit

---
+++ src/main/java/com/ms/userservicems/UserServiceMsApplication.java
@@ -0,0 +1,13 @@
+package com.ms.userservicems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/userservicems/UserServiceMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.userservicems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
